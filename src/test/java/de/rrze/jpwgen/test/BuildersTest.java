f10a88a96270619131686f439f2557a7352bd6d3
zhelev <zhelev@50736109-da4e-0410-a98c-8efb4efeaf9f>
2011-04-27 11:14:45 +0000

    - flag dependencies test

--- src/test/java/de/rrze/jpwgen/test/BuildersTest.java
+++ src/test/java/de/rrze/jpwgen/test/BuildersTest.java
@@ -2,10 +2,17 @@ package de.rrze.jpwgen.test;
 
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.apache.maven.surefire.shade.org.apache.commons.lang.time.StopWatch;
 import org.testng.annotations.Test;
 
+import de.rrze.jpwgen.IPwGenConstants;
+import de.rrze.jpwgen.flags.Only1SymbolFlag;
 import de.rrze.jpwgen.flags.PwGeneratorFlagBuilder;
+import de.rrze.jpwgen.flags.PwNumeralsFlag;
+import de.rrze.jpwgen.flags.PwReducedSymbolsFlag;
+import de.rrze.jpwgen.flags.PwSymbolsFlag;
 import de.rrze.jpwgen.impl.PwGenerator;
 import de.rrze.jpwgen.options.PwGeneratorOptionBuilder;
 import de.rrze.jpwgen.utils.PwHelper;
@@ -68,6 +75,47 @@ public class BuildersTest extends PwGeneratorTest
 		System.out.println("\nOPTIONS BUILDER TEST FINISHED Runtime:"
 				+ stopWatch.toString() + "\n");
 
+	}
+
+	@Test(groups =
+	{ "default" }, invocationCount = 20)
+	public void dependencyTest()
+	{
+		PwGeneratorFlagBuilder flagBuilder = new PwGeneratorFlagBuilder(
+				IPwGenConstants.DEFAULT_FLAGS);
+		int flags = flagBuilder.build();
+
+		flags = new Only1SymbolFlag().mask(flags);
+
+		Assert.assertFalse(new PwSymbolsFlag().isMasked(flags));
+		System.out.println("Symbols set: "
+				+ new PwSymbolsFlag().isMasked(flags));
+
+		Assert.assertTrue(new PwReducedSymbolsFlag().isMasked(flags));
+		System.out.println("Reduced Symbols set: "
+				+ new PwReducedSymbolsFlag().isMasked(flags));
+
+		flags = new PwSymbolsFlag().unmask((flags));
+		System.out.println("Symbols set: " + flags);
+
+		Assert.assertFalse(new PwSymbolsFlag().isMasked(flags));
+		Assert.assertTrue(new PwReducedSymbolsFlag().isMasked(flags));
+		Assert.assertTrue(new Only1SymbolFlag().isMasked(flags));
+
+		System.out.println("Only 1 Symbol set: "
+				+ new Only1SymbolFlag().isMasked(flags));
+
+		flags = new PwReducedSymbolsFlag().unmask(flags);
+		System.out.println("Reduced Symbols unset: " + flags);
+
+		Assert.assertFalse(new PwReducedSymbolsFlag().isMasked(flags));
+		Assert.assertFalse(new Only1SymbolFlag().isMasked(flags));
+
+		System.out.println("Only 1 Symbol set: "
+				+ new Only1SymbolFlag().isMasked(flags));
+
+		System.out.println("Numerals set: "
+				+ new PwNumeralsFlag().isMasked(flags));
 	}
 
 }
